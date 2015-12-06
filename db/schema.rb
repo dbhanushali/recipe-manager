8aa4955f2812ecae5e7f3ee4216195d00c0f4945
Dipen Bhanushali <dipen.bhanushali@gmail.com>
2015-12-06 20:30:08 +0530

    created food_prefernces model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206143557) do
+ActiveRecord::Schema.define(version: 20151206145933) do
+
+  create_table "food_preferences", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "food_types", force: :cascade do |t|
     t.string   "name",       limit: 255
