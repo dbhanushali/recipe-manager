230e49b832107c707cd4a7208f64345d44b0feb2
Dipen Bhanushali <dipen.bhanushali@gmail.com>
2015-12-07 02:18:46 +0530

    setup of the layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    authenticated :user do
+      root 'recipes#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   devise_for :users
   resources :recipes
 end
