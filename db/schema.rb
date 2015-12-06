ceecf7aa2e5402f8b1b38cec50a74be5f25e6c74
Dipen Bhanushali <dipen.bhanushali@gmail.com>
2015-12-06 20:23:21 +0530

    created food type model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151206143557) do
+
+  create_table "food_types", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
